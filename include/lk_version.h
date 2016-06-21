e67c246544798da09b585e0a01a7ee4a8e6c279d
Paolo Burgio <paolo.burgio@unimore.it>
2016-06-21 08:15:02 +0200

    Added LK_VERSION.H

---
+++ include/lk_version.h
@@ -0,0 +1,11 @@
+#ifndef __LK_VERSION_H__
+#define __LK_VERSION_H__
+
+#define LK_VERSION_MAJOR		0
+#define LK_VERSION_MINOR		1
+
+#define LK_CONTACT_NAME			"Paolo Burgio"
+#define LK_CONTACT_ORG			"HiPeRT Lab -- University of Modena and Reggio Emilia, Italy"
+#define LK_CONTACT_EMAIL		"paolo.burgio@unimore.it"
+
+#endif /*  __LK_VERSION_H__ */
